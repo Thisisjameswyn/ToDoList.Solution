830aa49616984fe6af909173fc85f9f84e63795b
James Wyn <thisisjameswyn@gmail.com>
2021-03-08 09:55:37 -0800

    add files

---
+++ README.md
@@ -0,0 +1,43 @@
+## James Wyn
+
+# Pierre's Bakery Order Page
+
+
+### Description
+This is my week 2 C# project for Epicodus.  The objective is to make a console application that allows a user to add a vendor and then place an order for said vendor.  The user can navigate different entered vendors and see the placed orders for each.
+
+### Technologies Used
+* C#
+* .Net Core 5.0
+* MSTest
+* DOTNET
+
+## Setup/Installation Requirements
+
+From the web:
+
+    Click the "Code" and click the 'Download zip' option.
+    Unzip the file
+    Application code is in the 'BakeryOrder' folder
+    Test Code is in the 'BakeryOrder.Tests' folder
+    Open any file to view the code
+
+From the Terminal:
+
+    You need to have .net Core 5.0 installed on your machine in order to run this program.
+    Clone the repository from GitHub using git clone https://github.com/Thisisjameswyn/BakeryOrder.Solution
+    Navigate to the downloaded folder using the cd command
+    To run the application, navigate to the BakeryOrder folder, and type dotnet restore then dotnet run in the terminal.
+    To run the tests, navigate to the BakeryOrder.Tests folder, and type dotnet test in the terminal.
+
+
+* * *
+
+## Link where to find my GitHub link
+
+*https://github.com/Thisisjameswyn/BakeryOrder.Solution
+
+
+## License
+[MIT License](https://opensource.org/licenses/MIT)
+&copy; James Wyn 2021
---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,88 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
+    [TestMethod]
+    public void ItemConstractor_CreateInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog."; //arrange
+      Item newItem = new Item(description);
+      string result = newItem.Description; //Act
+      Assert.AreEqual(description, result); //assert
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog";
+      Item newItem = new Item(description);
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> { }; // Arrange
+      List<Item> result = Item.GetAll(); // Act
+      CollectionAssert.AreEqual(newList, result); // Assert
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "Walk the dog"; //Arrange
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+      List<Item> result = Item.GetAll(); //Act
+      CollectionAssert.AreEqual(newList, result); //Assert
+    }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+			string description = "Walk the dog";
+			Item newItem = new Item(description);
+			int result = newItem.Id;
+			Assert.AreEqual(1, result);
+		}
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      //Act
+      Item result = Item.Find(2);
+
+      //Assert
+      Assert.AreEqual(newItem2, result);
+    }
+  }
+}
