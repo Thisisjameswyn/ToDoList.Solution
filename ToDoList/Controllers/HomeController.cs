9fddebfa4dfaab2dcc80b2b5b9a4724891f40e30
James Wyn <thisisjameswyn@gmail.com>
2021-03-10 14:48:30 -0800

    add working file structure for todolist

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -2,18 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
-    }
 
-    [Route("/favorite_photos")]
-    public ActionResult FavoritePhotos()
-    {
-      return View();
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
     }
-  }
 }
